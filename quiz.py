48df6945c228db4a7fe9a3abc05ce8a7707b4eaa
bigmoneyswAg69 <37856146+bigmoneyswAg69@users.noreply.github.com>
2018-06-26 20:45:55 +1000

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,236 @@
+print ('WELCOME TO THE MULTIPLE CHOICE TEST\n')
+name = input('WHAT IS YOUR NAME? ')
+print ('\nHI THERE ' + name + '! LET''S PLAY A GAME!\n')
+print ('I will ask you 10 questions and give you three choices for each question.\n\nYou select which choice is the correct answer. Eg. A, B or C\n')
+print ('Important : Please keep your CAPS LOCK on')
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                          SET THE SCORE TO ZERO                          #
+###########################################################################
+
+score = 0
+score = int(score)  #Convert the 0 into a number so we can add scores
+
+
+###########################################################################
+#                           QUESTION 1                                    #
+###########################################################################
+
+print ('QUESTION 1: what is the capital of france?\n')
+print ('A. Amsterdam')
+print ('B. Paris')
+print ('C. Canberra')
+print ('')
+
+Q1answer = "B"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 2                                    #
+###########################################################################
+
+print ('QUESTION 2: What is the capital of the USA?\n')
+print ('A. District of Columbia')
+print ('B. Washington')
+print ('C. Caliornia')
+print ('')
+
+Q2answer = "A"
+Q2response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+
+###########################################################################
+#                           QUESTION 3                                    #
+###########################################################################
+
+print ('QUESTION 3: What is the capital of Iran?\n')
+print ('A. Tehran')
+print ('B. Esfahan')
+print ('C. Kabul')
+print ('')
+
+Q3answer = "A"
+Q3response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 4                                    #
+###########################################################################
+
+print ('QUESTION 4: What is the capital of Germany?\n')
+print ('A. Berlin')
+print ('B. Istanbul')
+print ('C. Hamburg')
+print ('')
+
+Q4answer = "A"
+Q4response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 5                                    #
+###########################################################################
+
+print ('QUESTION 5: What is the capital of South Sudan?\n')
+print ('A. Juba')
+print ('B. Asmara')
+print ('C. Mogadishu')
+print ('')
+
+Q5answer = "A"
+Q5response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 6                                    #
+###########################################################################
+
+print ('QUESTION 6: What is the capital of Egypt?\n')
+print ('A. Cairo')
+print ('B. Siwah')
+print ('C. Aswan')
+print ('')
+
+Q6answer = "A"
+Q6response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 7                                    #
+###########################################################################
+
+print ('QUESTION 7: What is the capital of Japan?\n')
+print ('A. Tokyo')
+print ('B. Hiroshima')
+print ('C. Nagasaki')
+print ('')
+
+Q7answer = "A"
+Q7response= input('Your answer : ')
+
+if (Q7response != Q7answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q7response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 8                                    #
+###########################################################################
+
+print ('QUESTION 8: What is the capital of Indonesia?\n')
+print ('A. Jakarta')
+print ('B. Manila')
+print ('C. Kuala Lumpur')
+print ('')
+
+Q8answer = "A"
+Q8response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 9                                    #
+###########################################################################
+
+print ('QUESTION 9: What is the capital of China?\n')
+print ('A. Beijing')
+print ('B. Hong Kong')
+print ('C. Seoul')
+print ('')
+
+Q9answer = "A"
+Q9response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
+###########################################################################
+#                           QUESTION 10                                    #
+###########################################################################
+
+print ('QUESTION 10: What is the capital of Turkey?\n')
+print ('A. Istanbul')
+print ('B. Ankara')
+print ('C. Izmir')
+print ('')
+
+Q10answer = "A"
+Q10response= input('Your answer : ')
+
+if (Q2response != Q2answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q2response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10') 
+print ('\n-----------------------------------------------------------\n')
+
